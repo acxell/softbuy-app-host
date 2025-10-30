915eb0e69eabff79a378019ff54c39e5d9d2f70f
Acxell <acxellrizada@gmail.com>
2025-10-30 14:42:59 +0700

    Fix Build Error [add vercel]

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/softbuy-react/",
-  plugins: [
-    react(),
-    nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
-});
+  plugins: [react()],
+  base: '/',
+  build: {
+    sourcemap: true
+  }
+})
